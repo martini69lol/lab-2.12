b61b10f7f037646ce7e4b2d6ca0c307282bc0394
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-14 18:20:08 +0300

    Update individual.py

--- individual.py
+++ individual.py
@@ -2,19 +2,22 @@
 # # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
-  
-def sum_numbers_decorator(start):
-    def sum_numbers(func):
-        def wrapper(string):
-            numbers = list(map(int, string.split()))
-            return start + func(numbers)
-        return wrapper
-    return sum_numbers
 
 
-@sum_numbers_decorator(start=5)
-def sum_numbers(numbers):
-    return sum(numbers)
+    def sum_numbers_decorator(start):
+        def sum_numbers(func):
+            def wrapper(string):
+                numbers = list(map(int, string.split()))
+                return start + func(numbers)
+
+            return wrapper
+
+        return sum_numbers
+
+
+    @sum_numbers_decorator(start=5)
+    def sum_numbers(numbers):
+        return sum(numbers)
 
 
 input_string = input("Введите строку целых чисел через пробел: ")
